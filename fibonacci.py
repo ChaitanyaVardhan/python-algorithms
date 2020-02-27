ba110bf58affcfff4203f5fc861bcaadfb2cba8f
ChaitanyaVardhan <chaitanyavardhan1@gmail.com>
2020-02-27 17:19:41 +0530

    fixes and fibonacci bottom up

--- fibonacci.py
+++ fibonacci.py
@@ -12,14 +12,25 @@ def fib_n(n):
 
 def fib_memoize(n, memoize):
     if n == 1 or n == 2:
-        memoize[n-1] = n
-    if memoize[n-1]:
+        result = 1
+        memoize[n-1] = result
+    elif memoize[n-1]:
         result = memoize[n-1]
     else:
         result = fib_memoize(n-1, memoize) + fib_memoize(n-2, memoize)
         memoize[n-1] = result
     return result
     
+
+def fib_bottom_up(n):
+    fib_arr = [None] * n
+    fib_arr[0] = 1
+    fib_arr[1] = 1
+    i = 2
+    while i < n:
+        fib_arr[i] = fib_arr[i-1] + fib_arr[i-2]
+        i += 1
+    return fib_arr[n-1]
     
 if __name__ == "__main__":
     n = int(sys.argv[1])
@@ -34,3 +45,8 @@ if __name__ == "__main__":
     y = fib_memoize(n, memoize)
     end = time.time()
     print(f"fib_memoize {n} = {y} Time taken = {end-start}")
+
+    start = time.time()
+    y = fib_bottom_up(n)
+    end = time.time()
+    print(f"fib_bottom_up {n} = {y}  Time taken = {end-start}")
